3fe7cf9ffaad1fe06fb2d8a1a2c1a5e277e55e5d
Steve Biedermann <hardliner66@gmail.com>
2025-09-12 01:07:30 +0200

    use cargo to run test

--- test.ps1
+++ test.ps1
@@ -24,20 +24,20 @@ $env:Path = ".\target\debug\;" + $env:Path
 cargo build -p gxt-cli
 
 # Create keys for communication
-gxt keygen --out tmp/alice.key
-gxt keygen --out tmp/bob.key
+cargo run -p gxt-cli -q -- keygen --out tmp/alice.key
+cargo run -p gxt-cli -q -- keygen --out tmp/bob.key
 
 # Create an id card for bob
-Write-Output '{"name":"Bob"}' | gxt id tmp/bob.key --out tmp/bob.id --meta -
+Write-Output '{"name":"Bob"}' | cargo run -p gxt-cli -q -- id tmp/bob.key --out tmp/bob.id --meta -
 
 # Verify if the id card is valid and signed
-gxt verify --file tmp/bob.id
+cargo run -p gxt-cli -q -- verify --file tmp/bob.id
 
 # Create a message for bob using their id card and your own key
-gxt msg --key tmp/alice.key --to tmp/bob.id --out tmp/msg_to_bob.gxt --payload '{"hello":"world"}'
+cargo run -p gxt-cli -q -- msg --key tmp/alice.key --to tmp/bob.id --out tmp/msg_to_bob.gxt --payload '{"hello":"world"}'
 
 # Verify if the message is valid and signed
-gxt verify --file tmp/msg_to_bob.gxt
+cargo run -p gxt-cli -q -- verify --file tmp/msg_to_bob.gxt
 
 # Decrypt the message using bobs key
-gxt decrypt --key tmp/bob.key --file tmp/msg_to_bob.gxt
+cargo run -p gxt-cli -q -- decrypt --key tmp/bob.key --file tmp/msg_to_bob.gxt
